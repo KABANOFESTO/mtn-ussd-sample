e4bd294410b6bb639c35c5372ff0c2dc7d3d2d58
KABANOFESTO <kabanofesto1@gmail.com>
2025-09-29 01:29:02 +0200

    chore:add security

---
+++ src/main/java/com/ussd/mtn/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ussd.mtn.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(
+                        request -> new org.springframework.web.cors.CorsConfiguration().applyPermitDefaultValues())) // Enable
+                                                                                                                     // CORS
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/ussd/process",
+                                "/ussd/admin/menu",
+                                "/ussd/**",
+                                "/code**/",
+                                "/public/**")
+                        .permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
